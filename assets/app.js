745cd5553b99ad9f39a225a98b301ee8eb169089
Laika-Beats <laikabeatsdenver@gmail.com>
2021-01-14 13:54:26 -0700

    search function

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,18 @@
 const auth = "563492ad6f91700001000001dad42820d13a4564af0a88ab82c69e51";
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
-const submitButton = document.querySelector(".submit-btn");
+const form = document.querySelector(".search-form");
 let searchValue;
+
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  searchPhotos(searchValue);
+});
+
+function updateInput(event) {
+  searchValue = event.target.value;
+}
 
 async function curatedPhotos() {
   const dataFetch = await fetch(
@@ -27,3 +37,25 @@ async function curatedPhotos() {
 }
 
 curatedPhotos();
+
+async function searchPhotos(query) {
+  const dataFetch = await fetch(
+    `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    }
+  );
+  const data = await dataFetch.json();
+  console.log(data);
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+    <p>${photo.photographer}</p>`;
+    gallery.appendChild(galleryImg);
+  });
+}
