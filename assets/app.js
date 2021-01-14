6b55848a2f0a8ee0e57aecf183c9fbab0ebde037
Laika-Beats <laikabeatsdenver@gmail.com>
2021-01-14 13:25:46 -0700

    API fetch and data generation

---
+++ assets/app.js
@@ -0,0 +1,29 @@
+const auth = "563492ad6f91700001000001dad42820d13a4564af0a88ab82c69e51";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const submitButton = document.querySelector(".submit-btn");
+let searchValue;
+
+async function curatedPhotos() {
+  const dataFetch = await fetch(
+    "https://api.pexels.com/v1/curated?per_page=15&page=1",
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    }
+  );
+  const data = await dataFetch.json();
+  console.log(data);
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+    <p>${photo.photographer}</p>`;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+curatedPhotos();
